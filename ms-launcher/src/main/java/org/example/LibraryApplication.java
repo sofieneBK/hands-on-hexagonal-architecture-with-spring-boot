846a1d0af0b8b755490d04a9c034bbda60e23e3f
Sofiene BEN KHEMIS <sofianee.007@gmail.com>
2021-05-10 18:49:43 +0200

    Health

--- ms-launcher/src/main/java/org/example/LibraryApplication.java
+++ ms-launcher/src/main/java/org/example/LibraryApplication.java
@@ -2,12 +2,18 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String health() {
+		return  "HEALTH CHECK OK!";
 	}
 
 }
